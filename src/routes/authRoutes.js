f4425bc79ddc36a98350c7c1a3c2120b9a3db03d
Condition00 <anant.kavuru@gmail.com>
2025-06-21 10:19:03 +0530

    auth routes

---
+++ src/routes/authRoutes.js
@@ -0,0 +1,80 @@
+import express from 'express';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import prisma from '../prismaClient.js';
+
+const router = express.Router();
+
+// Register route
+router.post('/register', async (req, res) => {
+    const { username, password } = req.body;
+    // save username and encrypted password to database
+
+    //encrypt password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    //save new user to database and hashed password aswell
+    try {
+        const user = await prisma.user.create({
+            data: {
+                username,
+                password: hashedPassword
+            }
+    })
+
+    // now we have a user, I add their first todo item
+
+    const defaultTodo = `First todo for ${username}`;
+    await prisma.todo.create({
+        data: {
+            title: defaultTodo,
+            userId: user.id
+        }
+    })
+
+    // Generate JWT token
+    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: '24h' });
+    res.json({ token})
+
+    } catch (error) {
+        console.log(err.message);
+        res.sendStatus(503);
+    }
+})
+
+router.post('/login', async (req, res) => {
+     // we get their email, and we look up the password associated with that email in the database
+     // but we get it back and see it's encrypted, which means that we cannot compare it to the one the user just used trying to login
+     // so what we can to do, is again, one way encrypt the password the user just entered
+
+    const { username, password } = req.body;
+
+    try {
+        const user = await prisma.user.findUnique({
+            where: {
+                username: username
+            }
+        })
+
+        if (!user) {
+            return res.status(401).json({ message: 'Invalid username or password' });
+        }
+
+        const isPasswordValid = bcrypt.compareSync(password, user.password);
+
+        if (!isPasswordValid) {
+            return res.status(401).json({ message: 'Invalid username or password' });
+        }
+        console.log(user)
+
+        // Generate JWT token
+        const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: '24h' });
+        res.json({ token });
+
+    } catch (error) {
+        console.log(err.message);
+        res.sendStatus(503);
+    }
+});
+
+export default router;
---
+++ src/server.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import path, {dirname} from 'path';
+import {fileURLToPath} from 'url';
+
+
+const app = express();
+const PORT = process.env.PORT || 5003;
+
+// get file path from url of the current module
+const __filename = fileURLToPath(import.meta.url);
+// get directory name from file path
+const __dirname = dirname(__filename);
+
+//m
+app.use(express.json())
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+//rotes
+
+app.get('/auth', authRoutes);
+app.use('/todos',authMiddleware, todoRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
